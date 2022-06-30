00bf98519667b34003468644c6b1a7a1b26d76d0
Karel <30099757+Karel911@users.noreply.github.com>
2022-06-30 14:16:42 +0900

    minor updates

--- inference.py
+++ inference.py
@@ -14,6 +14,7 @@ from dataloader import get_test_augmentation, get_loader
 from model.TRACER import TRACER
 from util.utils import load_pretrained
 
+
 class Inference():
     def __init__(self, args, save_path):
         super(Inference, self).__init__()
@@ -59,31 +60,30 @@ class Inference():
 
                     # Save prediction map
                     if self.args.save_map is not None:
-                        output = (output.squeeze().detach().cpu().numpy() * 255.0).astype \
-                            (np.uint8)  # convert uint8 type
-                        
-                        salient_object = self.post_processing(images[i], output, H, W)
+                        output = (output.squeeze().detach().cpu().numpy() * 255.0).astype(np.uint8)
+
+                        salient_object = self.post_processing(images[i], output, h, w)
                         cv2.imwrite(os.path.join('mask', self.args.dataset, image_name[i] + '.png'), output)
                         cv2.imwrite(os.path.join('object', self.args.dataset, image_name[i] + '.png'), salient_object)
 
         print(f'time: {time.time() - t:.3f}s')
 
     def post_processing(self, original_image, output_image, height, width, threshold=200):
-        invTrans = transforms.Compose([ transforms.Normalize(mean = [ 0., 0., 0. ],
-                                                            std = [ 1/0.229, 1/0.224, 1/0.225 ]),
-                                        transforms.Normalize(mean = [ -0.485, -0.456, -0.406 ],
-                                                            std = [ 1., 1., 1. ]),
-                                    ])
+        invTrans = transforms.Compose([transforms.Normalize(mean=[0., 0., 0.],
+                                                            std=[1 / 0.229, 1 / 0.224, 1 / 0.225]),
+                                       transforms.Normalize(mean=[-0.485, -0.456, -0.406],
+                                                            std=[1., 1., 1.]),
+                                       ])
         original_image = invTrans(original_image)
 
         original_image = F.interpolate(original_image.unsqueeze(0), size=(height, width), mode='bilinear')
-        original_image = (original_image.squeeze().permute(1,2,0).detach().cpu().numpy() * 255.0).astype(np.uint8)
+        original_image = (original_image.squeeze().permute(1, 2, 0).detach().cpu().numpy() * 255.0).astype(np.uint8)
 
         rgba_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2BGRA)
         output_rbga_image = cv2.cvtColor(output_image, cv2.COLOR_BGR2BGRA)
 
-        output_rbga_image[:, :, 3] = output_image                     # Extract edges
+        output_rbga_image[:, :, 3] = output_image  # Extract edges
         edge_y, edge_x, _ = np.where(output_rbga_image <= threshold)  # Edge coordinates
-        
+
         rgba_image[edge_y, edge_x, 3] = 0
         return cv2.cvtColor(rgba_image, cv2.COLOR_RGBA2BGRA)
