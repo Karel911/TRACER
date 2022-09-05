7b76734dfa093ba12c1135635dcfbb5159d4eaf9
Karel <30099757+Karel911@users.noreply.github.com>
2022-09-05 23:51:35 +0900

    demo_init

---
+++ demo_run.sh
@@ -0,0 +1,11 @@
+#TRACER
+#├── data
+#│   ├── custom_dataset
+#│   │   ├── sample_image1.png
+#│   │   ├── sample_image2.png
+#      .
+#      .
+#      .
+
+# For testing TRACER with pre-trained model (e.g.)
+python main.py inference --dataset custom_dataset/ --arch 7 --img_size 640 --save_map True
